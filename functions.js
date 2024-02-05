9e7e46af7417209c2a20e12cc0f95bb8248e7d2e
Abdelkrim-eljaouhari <mustaphason1991@outlook.fr>
2024-02-05 10:56:23 +0100

    arrow and function expression

---
+++ functions.js
@@ -0,0 +1,3 @@
+let animals = ["lion", "heyenas", "leopard", "elephant", "dog"];
+let animalsNameLength = animals.map((animal) => animal.length);
+console.log(animalsNameLength); //[ 4, 7, 7, 8, 3 ]
