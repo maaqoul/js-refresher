3f7e1fc4550a26e2283bb8fd82d15935efb15de8
Fethi TALBI <ftalbi@sqli.com>
2024-02-05 09:57:36 +0100

    Exercice: let and const

---
+++ TALBI_FETHI/letAndConst.js
@@ -0,0 +1,13 @@
+//Let
+for(let i = 1; i < 10; i++) {
+    // Do somthing
+}
+console.log(i);
+
+// Const
+const car = {color : 'blue', power: 761};
+console.log(car);
+
+// After
+car.color = 'black';
+console.log(car);
