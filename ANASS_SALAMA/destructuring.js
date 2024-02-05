b5aa3aa0043936f5d5a83d45c633c67b584f2f21
asalama <anassalama10349@gmail.com>
2024-02-05 11:55:57 +0100

    literals & destructuring

---
+++ ANASS_SALAMA/destructuring.js
@@ -0,0 +1,13 @@
+// first assignement
+const person = { firstName: 'John', lastName: 'Doe' };
+const { firstName, lastName } = person;
+
+console.log(firstName);
+console.log(lastName);
+
+// seconde assignement
+const arr = [1, 2, 3, 4, 5];
+const [a, , c] = arr;
+
+console.log(a);
+console.log(c);
