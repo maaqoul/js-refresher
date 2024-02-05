c15111239bed5ed65fe38d25f896efcceae72ed5
ImahrainMed <mohammedimahrain@hotmail.com>
2024-02-05 10:13:29 +0100

    first exersice

---
+++ IMAHRAIN_MOHAMMED/letconst.js
@@ -0,0 +1,13 @@
+// 1.
+for (i=0 ; i<10 ; i++) {
+    let a = 'hello from the loop';
+}
+console.log(a);
+
+// 2.
+
+const Animal = {name: 'shark', category: 'predator', environment: 'marine'};
+
+Animal.name = 'orca';
+
+console.log(Animal);
