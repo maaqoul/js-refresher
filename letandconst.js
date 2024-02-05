7b291e066c7c6b2a1316fdef81f315aaf06e0e7e
Abdelkrim-eljaouhari <mustaphason1991@outlook.fr>
2024-02-05 09:58:27 +0100

    letandconst c2

--- letandconst.js
+++ letandconst.js
@@ -1,11 +1,11 @@
 for (let i = 0; i < 10; i++) {
-    let value = "exercice1";
-  }
-  // console.log(value);
-  
-  const firstObj = {
-    name: "karim",
-    favoritesport: "football",
-  };
-  console.log(firstObj);
-  firstObj.
+  let value = "exercice1";
+}
+// console.log(value);
+
+const firstObj = {
+  name: "karim",
+  favoritesport: "football",
+};
+console.log(firstObj);
+firstObj.name;
